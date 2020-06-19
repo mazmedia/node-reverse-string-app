2194ba1af0476e0c8b613ec25750db19e1030b8a
Jivin Mazumder <jivinmazumder@debdeeps-air.lan>
2020-06-19 10:31:45 -0400

    reverse words using Javascript

--- public/js/main.js
+++ public/js/main.js
@@ -19,7 +19,24 @@ const main = () => {
     const __INPUT_VALUE__ = document.querySelector('#input').value;
     const __ACTUAL_HEADER__ = document.querySelector('#actual-header');
     const __ACTUAL__ = document.querySelector('#actual');
+    let __ARR_OF_WORDS__ = [];
+
+    console.log(__INPUT_VALUE__.length);
+
+    // for (let i = 0; i < __INPUT_VALUE__.length; i++) {
+    //     __ARR_OF_WORDS__ = [
+    //         ...__INPUT_VALUE__[i]
+    //     ];
+    //     let vary = __ARR_OF_WORDS__[i] + ((__INPUT_VALUE__.length) - i);
+    //     console.log(__ARR_OF_WORDS__);
+    //     console.log(vary)
+    // }
+
+    for (let i = __INPUT_VALUE__.length - 1; i >= 0; i--) {
+        __ARR_OF_WORDS__.push(__INPUT_VALUE__[i]);
+        console.log(__ARR_OF_WORDS__);
+    }
 
     __ACTUAL_HEADER__.innerHTML = 'Reversed:';
-    __ACTUAL__.innerHTML = __INPUT_VALUE__;
+    __ACTUAL__.innerHTML = __ARR_OF_WORDS__.join("");
 }
